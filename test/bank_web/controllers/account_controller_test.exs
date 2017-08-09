1e8f7690e4c3e4e6e2410fcf221f8910d2d4301e
Paulo Chaves <opaulochaves@gmail.com>
2017-08-09 11:01:33 -0300

    update test

--- test/bank_web/controllers/account_controller_test.exs
+++ test/bank_web/controllers/account_controller_test.exs
@@ -1,7 +1,14 @@
 defmodule BankWeb.AccountControllerTest do
   use BankWeb.ConnCase
 
-  @create_attrs %{number: "123", amount: "500.9", description: "some operation", type: "deposit", date: "2017-08-01T00:00:00"}
+  @operations [
+    %{number: "123", amount: "500", description: "some operation", type: "deposit", date: "2017-08-01T00:00:00"},
+    %{number: "123", amount: "1000", description: "some operation", type: "deposit", date: "2017-08-02T00:00:00"},
+    %{number: "123", amount: "150", description: "some operation", type: "purchase", date: "2017-08-01T00:00:00"},
+    %{number: "123", amount: "35", description: "some operation", type: "purchase", date: "2017-08-02T00:00:00"},
+    %{number: "123", amount: "220", description: "some operation", type: "withdrawal", date: "2017-08-05T00:00:00"},
+    %{number: "123", amount: "390", description: "some operation", type: "withdrawal", date: "2017-08-08T00:00:00"}
+  ]
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -16,13 +23,13 @@ defmodule BankWeb.AccountControllerTest do
 
   describe "creates an operation" do
     test "renders operation when data is valid", %{conn: conn} do
-      conn = post conn, account_path(conn, :create, "123"), operation: @create_attrs
+      conn = post conn, account_path(conn, :create, "123"), operation: List.first(@operations)
       assert %{"number" => number} = json_response(conn, 201)["data"]
 
       conn = get conn, account_path(conn, :index, number)
       assert json_response(conn, 200)["data"] == [%{
         "number" => number,
-        "amount" => 500.9,
+        "amount" => 500,
         "description" => "some operation",
         "type" => "deposit",
         "date" => "2017-08-01T00:00:00"
